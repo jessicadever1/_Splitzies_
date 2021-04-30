180c5b74fe70b0c4898c3cd1c0ca2be36f8f0113
Jess Dever <jessicadever1@gmail.com>
2021-04-30 15:48:35 -0500

    fixing paths in login, reg, header and appviews

---
+++ Splitzies/client/src/components/ApplicationViews.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { UserProfileContext, UserProfileProvider } from "./providers/UserProfileProvider";
+import Login from "./Login/Login"
+import Register from "./Login/Register"
+
+export default function ApplicationViews() {
+    const { isLoggedIn } = useContext(UserProfileContext);
+
+    return (
+        <main>
+            <Switch>
+
+                <Route exact path="/">
+
+                </Route>
+
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+
+                <Route exact path="/register">
+                    <Register />
+                </Route>
+
+            </Switch>
+        </main>
+    );
+};
---
+++ Splitzies/client/src/components/Header.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from 'react';
+import { NavLink as RRNavLink, useHistory } from "react-router-dom";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import { UserProfileContext } from "./providers/UserProfileProvider";
+
+export default function Header() {
+    const { isLoggedIn, logout } = useContext(UserProfileContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const userProfile = JSON.parse(sessionStorage.getItem("userProfile"));
+
+    return (
+        <div>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand tag={RRNavLink} to="/">Tabloid</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        { /* When isLoggedIn === true, we will render the Home and Post links */}
+                        {isLoggedIn &&
+                            <NavItem className="nav-items">
+                                <NavLink tag={RRNavLink} to="/">Splitz</NavLink>
+                                <NavLink tag={RRNavLink} to="/create">+</NavLink>
+                                <NavLink tag={RRNavLink} to="/balance">Balance</NavLink>
+                            </NavItem>
+                        }
+                        {isLoggedIn && userProfile.userTypeId == 1 &&
+                            <NavItem className="nav-items">
+                                <NavLink tag={RRNavLink} to="/tag">Tag Management</NavLink>
+                                <NavLink tag={RRNavLink} to="/userProfiles">User Profiles</NavLink>
+                            </NavItem>
+                        }
+                    </Nav>
+                    <Nav navbar>
+                        {isLoggedIn &&
+                            <>
+                                <NavItem>
+                                    <a aria-current="page" className="nav-link"
+                                        style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                                </NavItem>
+                            </>
+                        }
+                        {!isLoggedIn &&
+                            <>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/login">Login</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={RRNavLink} to="/register">Register</NavLink>
+                                </NavItem>
+                            </>
+                        }
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div >
+    );
+}
