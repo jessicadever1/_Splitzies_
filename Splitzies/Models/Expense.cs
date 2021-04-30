f3791e782e75a02d4476cd4d6906515621d8ecf8
Jess Dever <jessicadever1@gmail.com>
2021-04-30 11:42:51 -0500

    expense model

---
+++ Splitzies/Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Splitzies.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string ExpenseName { get; set; }
+        public int CategoryId { get; set; }
+        public int Amount { get; set; }
+        public int UserWhoPaidId { get; set; }
+        public int SplitzId { get; set; }
+        public DateTime DeletedDate { get; set; }
+    }
+}
