4a9ec2766fd65e9c0bdb7356dc0926c9bf822553
Jess Dever <jessicadever1@gmail.com>
2021-04-30 13:15:43 -0500

    utils, repos and startup

---
+++ Splitzies/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using Splitzies.Models;
+using System.Collections.Generic;
+
+namespace Splitzies.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        void Deactivate(int id);
+        List<UserProfile> GetAllUserProfiles();
+        UserProfile GetByFirebaseUserId(string firebaseUserId);
+        UserProfile GetById(int id);
+    }
+}
