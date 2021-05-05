cb85d6a7a3c162f9e9890c488e66a05284037c48
Jess Dever <jessicadever1@gmail.com>
2021-05-05 17:27:31 -0500

    all inputs entered

--- Splitzies/client/src/components/Splitz/SplitzAdd.js
+++ Splitzies/client/src/components/Splitz/SplitzAdd.js
@@ -28,31 +28,78 @@ export const SplitzAdd = () => {
     // }
 
     return (<>
-        <Form className="padding">
+        <Form className="padding seeBot">
             <h1>Let's Add A Splitz!</h1>
             <Input
                 id="splitzName"
+                className="margBot"
                 type="text"
-                placeholder="Name of Your New Splitz">
+                placeholder="Name Your Splitz">
             </Input>
             <Input
                 id="date"
+                className="margBot"
                 type="date">
             </Input>
             <Input
                 id="splitzPic"
+                className="margBot"
                 type="text"
                 placeholder="Image URL">
             </Input>
             <Input
                 id="splitzDetails"
                 type="textarea"
-                className="textarea"
+                className="textarea margBot"
                 placeholder={`Dear Future You,\
-                You may not remember what this splitz was all about. So I’ve written a few notes about it to jog your memory.\
+                \ You may not remember what this splitz was all about. So I’ve written a few notes about it to jog your memory.\
                 Love,\
-                Current You`}>
+                \ Current You`}>
             </Input>
+            <Input
+                id="searchForUser"
+                className="margBot"
+                type="search"
+                placeholder="Who is Spitzing with you?">
+            </Input>
+            <div>Now, let's add your expenses!</div>
+            <Input
+                id="expenseName"
+                placeholder="Expense Name"
+                className="margBot"
+                type="text">
+            </Input>
+            <div className="flexRow jc">
+                <div>$</div>
+                <div>
+                    <Input
+                        id="amount"
+                        type="number"
+                        placeholder="Dollar Amount"
+                        className="margBot">
+                    </Input>
+                </div>
+            </div>
+            <Input
+                id="category"
+                type="select"
+                placeholder="Expense Category"
+                className="margBot">
+            </Input>
+            <Input
+                id="paidBy"
+                type="select"
+                placeholder="Paid By"
+                className="margBot">
+            </Input>
+
+            <div className="flexRow">
+                <Button id="btn" className="margBot">Save Expense</Button>
+                <Button id="btn" className="margBot">Add Another Expense</Button>
+            </div>
+            <div class="center">
+                <Button id="btn" className="margBot">Savezies</Button>
+            </div>
         </Form>
     </>)
 };
