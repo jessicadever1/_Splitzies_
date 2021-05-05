7bafdbb7c48da10950ebb845493c7dcd11474e0d
Jess Dever <jessicadever1@gmail.com>
2021-05-05 15:34:13 -0500

    add button

--- Splitzies/client/src/components/Footer.js
+++ Splitzies/client/src/components/Footer.js
@@ -8,6 +8,8 @@ import {
 } from 'reactstrap';
 import { UserProfileContext } from "./providers/UserProfileProvider";
 import './footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 export const FooterMenu = () => {
     const { isLoggedIn, logout } = useContext(UserProfileContext);
@@ -15,12 +17,14 @@ export const FooterMenu = () => {
     return (
         <div>
 
-            <Nav className="footer" navbar>
+            <Nav className="footer padding8">
 
                 {isLoggedIn &&
-                    <NavItem className="nav-items pink flex">
+                    <NavItem className="nav-items pink flex m8">
                         <NavLink id="textColorWhite" tag={RRNavLink} to="/mySplitz">Splitz</NavLink>
-                        <NavLink id="textColorWhite" tag={RRNavLink} to="">+</NavLink>
+                        <NavLink id="textColorWhite" tag={RRNavLink} to="">
+                            <FontAwesomeIcon className="fontSize20" icon={faPlusCircle} />
+                        </NavLink>
                         <NavLink id="textColorWhite" tag={RRNavLink} to="">Balance</NavLink>
                     </NavItem>
                 }
