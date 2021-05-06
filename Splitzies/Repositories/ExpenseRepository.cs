f2fc9afbffd1933b22296726e34c9dd677d0620e
Jess Dever <jessicadever1@gmail.com>
2021-05-06 13:16:54 -0500

    expenses repo and irepo

---
+++ Splitzies/Repositories/ExpenseRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Splitzies.Models;
+using Splitzies.Utils;
+
+namespace Splitzies.Repositories
+{
+    public class ExpenseRepository : BaseRepository, IExpenseRepository
+    {
+        public ExpenseRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Expense> GetAllExpensesBySplitzId(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                        SELECT c.Id, c.PostId, c.UserProfileId, c.Subject, c.Content, c.CreateDateTime,
+                                               p.Id,                                    
+                                               u.Id AS userId
+                                        FROM Comment c
+                                        JOIN Post p ON c.PostId = p.Id
+                                        JOIN UserProfile u ON c.UserProfileId = u.Id
+                                        WHERE c.PostId = @id
+                                        ORDER BY c.CreateDateTime DESC
+                                        ";
+                    DbUtils.AddParameter(cmd, "@id", id);
+                    var reader = cmd.ExecuteReader();
+                    var comments = new List<Comment>();
+                    while (reader.Read())
+                    {
+                        comments.Add(new Comment()
+                        {
+                            Id = DbUtils.GetInt(reader, "Id"),
+                            PostId = DbUtils.GetInt(reader, "PostId"),
+                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            Subject = DbUtils.GetString(reader, "Subject"),
+                            Content = DbUtils.GetString(reader, "Content"),
+                            CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTime"),
+                            userProfile = new UserProfile()
+                            {
+                                Id = DbUtils.GetInt(reader, "userId")
+                            }
+
+                        });
+                    }
+                    reader.Close();
+                    return comments;
+                }
+
+            }
+        }
+    }
+}
