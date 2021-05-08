c425c8fd95577f42ad22ce0f51430fb72e542e85
Jess Dever <jessicadever1@gmail.com>
2021-05-07 21:13:34 -0500

    logo

--- Splitzies/client/src/components/Login/Login.js
+++ Splitzies/client/src/components/Login/Login.js
@@ -2,7 +2,8 @@ import React, { useState, useContext } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useHistory, Link } from "react-router-dom";
 import { UserProfileContext } from "../providers/UserProfileProvider";
-import "./login.css"
+import "./login.css";
+import logo from "../images/splitzites.png"
 
 export default function Login() {
     const history = useHistory();
@@ -19,24 +20,27 @@ export default function Login() {
     };
 
     return (
-        <Form onSubmit={loginSubmit} className="p backgroundWhite">
-            <fieldset id="border" className="ptb">
-                <FormGroup className="p">
-                    <Label for="email">Email</Label>
-                    <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
-                </FormGroup>
-                <FormGroup className="p">
-                    <Label for="password">Password</Label>
-                    <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
-                </FormGroup>
-                <FormGroup id="noMarg" className="p jcc ">
-                    <Button id="btn">Login</Button>
-                </FormGroup>
-                <em className="p">
-                    <div className="jcc">Are you new here? Let's get you</div>
-                    <Link to="register" className="jcc mb">Registered!</Link>
-                </em>
-            </fieldset>
-        </Form>
+        <>
+            <img className="logoSize" src={logo}></img>
+            <Form onSubmit={loginSubmit} className="p backgroundWhite">
+                <fieldset id="border" className="ptb">
+                    <FormGroup className="p">
+                        <Label for="email">Email</Label>
+                        <Input id="email" type="text" onChange={e => setEmail(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup className="p">
+                        <Label for="password">Password</Label>
+                        <Input id="password" type="password" onChange={e => setPassword(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup id="noMarg" className="p jcc ">
+                        <Button id="btn">Login</Button>
+                    </FormGroup>
+                    <em className="p">
+                        <div className="jcc">Are you new here? Let's get you</div>
+                        <Link to="register" className="jcc mb">Registered!</Link>
+                    </em>
+                </fieldset>
+            </Form>
+        </>
     );
 }
