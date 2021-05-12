de08def85485595b6729905ab9bf2e32ae029835
Jess Dever <jessicadever1@gmail.com>
2021-05-11 20:47:11 -0500

    bal

--- Splitzies/client/src/components/Balances/Balance.js
+++ Splitzies/client/src/components/Balances/Balance.js
@@ -2,7 +2,9 @@ import React, { useEffect, useContext } from "react";
 import { Card, CardBody } from "reactstrap"
 import dateFormat from 'dateformat';
 import { Link } from "react-router-dom";
-import { SplitzContext } from "../providers/SplitzProvider"
+import { SplitzContext } from "../providers/SplitzProvider";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 export const Balance = ({ splitz }) => {
     const date = dateFormat(splitz.date, "mmmm dS, yyyy")
@@ -12,6 +14,9 @@ export const Balance = ({ splitz }) => {
         getMySplitzies()
     }, [])
 
+    let usersOnSplitz = splitz.userProfiles
+    let array = [1]
+
     return (
         <>
             <Card className="m-4">
@@ -19,9 +24,23 @@ export const Balance = ({ splitz }) => {
                     <Link className="wrapText" to={`/splitzDetails/${splitz.id}`}>{splitz.splitzName}</Link>
                     <div>{date}</div>
                     <div className="flexRow">
-                        <img className="balImg" src={splitz.userProfiles[0].profilePic} alt="profile pic"></img>
-                        <img className="balImg" src={splitz.userProfiles[1].profilePic} alt="profile pic"></img>
+                        {array.map((user) => {
 
+                            let filter = usersOnSplitz.filter(val => val.id)
+                            let numOfSplitzers = filter.length;
+
+                            if (numOfSplitzers === 1) {
+                                return (
+                                    <div className="center">
+                                        <img className="balImg" src={splitz.userProfiles[0].profilePic} alt="profile pic"></img>
+                                        <object><FontAwesomeIcon className="" icon={faArrowRight} /></object>
+                                        <small>$0</small>
+                                        <object><FontAwesomeIcon className="" icon={faArrowRight} /></object>
+                                        <img className="balImg" src={splitz.userProfiles[0].profilePic} alt="profile pic"></img>
+                                    </div>
+                                )
+                            }
+                        })}
                     </div>
                 </CardBody>
             </Card>
